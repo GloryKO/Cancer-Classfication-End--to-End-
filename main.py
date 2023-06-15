0811ad34634be1476a31e4ee01f241acddc6d524
GloryKO <koladeg2022@gmail.com>
2023-06-15 14:08:23 -0700

    finished with all utility functions

--- main.py
+++ main.py
@@ -1,17 +1,23 @@
 import os
 import logging
 
-# Create the "logs" directory if it doesn't exist
-os.makedirs("logs", exist_ok=True)
+# # Create the "logs" directory if it doesn't exist
+# os.makedirs("logs", exist_ok=True)
 
-# Configure the logging settings
-log_file = "logs/main.log"
-logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+# # Configure the logging settings
+# log_file = "logs/main.log"
+# logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
 
-# Perform your program logic
-# ...
+# # Perform your program logic
+# # ...
 
-# Write logs using the logging module
-logging.debug("This is a debug message")
+# # Write logs using the logging module
+# logging.debug("This is a debug message")
 
+def set_logging(message:str)->str:
+    os.makedirs("logs",exist_ok=True)
+    log_file = "logs/main.log"
+    logging.basicConfig(filename=log_file, level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s')
+    return logging.debug(message)
 
+set_logging("This is a debug message")
--- src/Cancer_Classifier/utils/common.py
+++ src/Cancer_Classifier/utils/common.py
@@ -1,7 +1,7 @@
 import os
 from box.exceptions import BoxValueError
 import yaml
-import logger
+#import logger
 import json
 import joblib
 from ensure import ensure_annotations
@@ -9,14 +9,15 @@ from box import ConfigBox
 from pathlib import Path
 from typing import Any
 import base64
+from main import set_logging
 
 
-@ensure_annotation #read yaml files
+@ensure_annotations #read yaml files
 def read_yaml(path_to_yaml:Path)->ConfigBox:
     try:
         with open(path_to_yaml) as yaml_file:
             content = yaml.safe_load(yaml_file)
-            #logger.info(f"yaml file: {path_to_yaml} loaded successfully")
+            set_logging(f"yaml file: {path_to_yaml} loaded successfully")
             return ConfigBox(content)
     except BoxValueError:
         raise ValueError("Empty yaml file")
@@ -29,10 +30,67 @@ def create_directories(path_to_directories:list,verbose=True):
     for path in path_to_directories:
         os.makedirs(path)
         if verbose:
-            logger.info(f"created directory at: {path}")
+            set_logging(f"created directory at: {path}")
 
 @ensure_annotations
 def save_json(path:Path,data:dict):
     with open(path, 'w') as f:
         json.dump(data, f, indent=4)
-    logger.info(f"json saved at: {path}")
+    set_logging(f"json saved at: {path}")
+
+@ensure_annotations
+def load_json(path:Path):
+    with open(path, 'r') as f:
+        content = json.load(f)
+    set_logging(f"json loaded at: {path}")
+    return ConfigBox(content)
+
+@ensure_annotations
+def save_bin(data:Any,path:Path):
+    """save binary file
+
+    Args:
+        data (Any): data to be saved as binary
+        path (Path): path to binary file
+    """
+    joblib.dump(value=data,filename=path)
+    set_logging(f"binary file saved at: {path}")
+
+@ensure_annotations
+def load_bin(path:Path):
+    """load binary data
+
+    Args:
+        path (Path): path to binary file
+
+    Returns:
+        Any: object stored in the file
+    """
+    data = joblib.load(path)
+    set_logging(f"binary data loaded from{path}")
+    return data
+
+@ensure_annotations
+def get_size(path:Path)->str:
+    """get size in KB
+
+    Args:
+        path (Path): path of the file
+
+    Returns:
+        str: size in KB
+    """
+    size_in_kb = round(os.path.getsize(path)/1024)
+    return f"~ {size_in_kb} KB"
+
+@ensure_annotations
+def decodeImage(imgstring,fileName):
+    imgdata = base64.b64decode(imgstring)
+    with open(fileName, 'wb') as f:
+        f.write(imgdata)
+        f.close()
+        
+@ensure_annotations
+def encodeImageIntoBase64(croppedImagePath):
+    with open(croppedImagePath, "rb") as f:
+        return base64.b64encode(f.read())
